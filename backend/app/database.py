c96f4c958476f11d71b1489a2265ac39fce85636
677-906 <tiemenijenny@gmail.com>
2025-09-16 11:13:04 +0100

    Mise à jour de la configuration de la base de données pour Render

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-POSTGRES_USER = "postgres"
-POSTGRES_PASSWORD = "carelle%402025"
-POSTGRES_SERVER = "localhost"  # ou l'adresse IP de votre serveur
-POSTGRES_PORT = "5432"
-POSTGRES_DB = "SP_db" # Le nom de la base de données que vous avez créée
+POSTGRES_USER = os.getenv("sp_database_user")
+POSTGRES_PASSWORD = os.getenv("RBhRyvmZdJKEeSROR6mo7U3GF04ba8q")
+POSTGRES_SERVER = os.getenv("dpg-d34ip4ruibrs73aipjrg-a") 
+POSTGRES_PORT = os.getenv("5432")
+POSTGRES_DB = os.getenv("sp_database") # Le nom de la base de données que vous avez créée
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:carelle%402025@localhost:5432/SP_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://sp_database_user:RBhRyvmZdJKEeSROR6mo7U3GF04ba8q@dpg-d34ip4ruibrs73aipjrg-a:5432/sp_database"
 # --- FIN DE LA CONFIGURATION ---
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
